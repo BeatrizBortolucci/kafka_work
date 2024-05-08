114a4a601c263dc046c14525849f5af1273e862b
H-Leone <henriqueleone.dev@gmail.com>
2024-05-08 07:40:43 -0300

    new producer

--- src/main/java/com/antifraud/msantifraud/controller/BeatrizBortolucciController.java
+++ src/main/java/com/antifraud/msantifraud/controller/BeatrizBortolucciController.java
@@ -1,14 +1,23 @@
 package com.antifraud.msantifraud.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/bibia")
 public class BeatrizBortolucciController {
+//    private final KafkaProducer producer;
+
     @GetMapping
     public String beatrizDizOi() {
         return "Olá, sou a Beatriz Bortolucci Domingos";
     }
+
+//    @PostMapping
+//    public void beatrizMandaMsg() {
+//        producer.publish("a", "vered_antifraude", "oi");
+//    }
 }
---
+++ src/main/java/com/antifraud/msantifraud/kafka/AntifraudProducer.java
@@ -0,0 +1,27 @@
+package com.antifraud.msantifraud.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class AntifraudProducer<T> {
+    private Producer<String, T> producer;
+
+    public AntifraudProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // replace with your Kafka brokers
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // replace with your serializer
+
+        this.producer = new KafkaProducer<String, T>(props);
+    }
+
+    public void send(String topic, T message) {
+        ProducerRecord<String, T> record = new ProducerRecord<>(topic, message);
+        producer.send(record);
+    }
+}
